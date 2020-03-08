2b334e71b4215b3e79fb3726f1138b73c2eec7a9
Qwaszxman <qwaszxman@gmail.com>
2020-03-08 15:41:23 +0000

    fetchData action

--- _/src/Services/shelf/actions.js
+++ _/src/Services/shelf/actions.js
@@ -16,32 +16,34 @@ const compare = {
   }
 };
 
-export const fetchProducts = (filters, sortBy, callback) => dispatch => {
-  return axios
-    .get(productsAPI)
-    .then(res => {
-      let { products } = res.data;
+const fetchDataAction = (filters, sortBy, callback) => {
 
-      if (!!filters && filters.length > 0) {
-        products = products.filter(p =>
-          filters.find(f => p.availableSizes.find(size => size === f))
-        );
-      }
+};
 
-      if (!!sortBy) {
-        products = products.sort(compare[sortBy]);
-      }
+export const fetchProducts = (filters, sortBy, callback) => async (dispatch) => {
 
-      if (!!callback) {
-        callback();
-      }
+  // export const fetchProducts = async (filters, sortBy, callback) => {
+  // export const fetchProducts = async (filters, sortBy, callback) => {
 
-      return dispatch({
-        type: FETCH_PRODUCTS,
-        payload: products
-      });
-    })
-    .catch(err => {
-      console.log('Could not fetch products. Try again later.');
-    });
+  const data = await fetch(productsAPI);
+  const dataJSON = await data.json();
+
+  let { products } = dataJSON;
+
+  if (!!filters && filters.length > 0) {
+    products = products.filter(p =>
+      filters.find(f => p.availableSizes.find(size => size === f))
+    );
+  }
+
+  if (!!sortBy) {
+    products = products.sort(compare[sortBy]);
+  }
+
+  if (!!callback) {
+    callback();
+  }
+
+  // return { type: FETCH_PRODUCTS, payload: products };
+  return dispatch({ type: FETCH_PRODUCTS, payload: products });
 };
