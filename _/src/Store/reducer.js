d2af9422a08478fd6c7823a79aa900c9737323d2
Qwaszxman <qwaszxman@gmail.com>
2020-03-08 01:35:00 +0000

    add cart and total reducers

--- _/src/Store/reducer.js
+++ _/src/Store/reducer.js
@@ -1,7 +1,17 @@
 import { FETCH_PRODUCTS } from '../Services/shelf/actionTypes';
+import { LOAD_CART, ADD_PRODUCT, REMOVE_PRODUCT, CHANGE_PRODUCT_QUANTITY } from '../Services/shelf/actionTypes';
+import { UPDATE_CART } from '../Services/total/actionTypes';
+
 
 const intialState = {
     products: [],
+    data: {
+        productQuantity: 0,
+        installments: 0,
+        totalPrice: 0,
+        currencyId: 'USD',
+        currencyFormat: '$'
+    }
 }
 
 const reducer = (state = intialState, action) => {
@@ -9,7 +19,33 @@ const reducer = (state = intialState, action) => {
         case FETCH_PRODUCTS:
             return {
                 ...state,
-                state['products']: action.payload
+                products: action.payload
+            };
+
+        case LOAD_CART:
+            return {
+                ...state,
+                products: action.payload
+            };
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                productToAdd: Object.assign({}, action.payload)
+            };
+        case REMOVE_PRODUCT:
+            return {
+                ...state,
+                productToRemove: Object.assign({}, action.payload)
+            };
+        case CHANGE_PRODUCT_QUANTITY:
+            return {
+                ...state,
+                productToChange: Object.assign({}, action.payload)
+            };
+        case UPDATE_CART:
+            return {
+                ...state,
+                data: action.payload
             };
         default:
             return state;
